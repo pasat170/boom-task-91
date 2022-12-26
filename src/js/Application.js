facc999f0f2da1300622588677087e15b44d2b4a
Stefan Poibrenski <stefan_poibrenski@abv.bg>
2022-12-27 00:23:18 +0200

    feat: add message for lyrics

--- src/js/Application.js
+++ src/js/Application.js
@@ -2,24 +2,29 @@ import EventEmitter from "eventemitter3";
 import Beat from "./Beat";
 
 export default class Application extends EventEmitter {
-  static get events() {
-    return {
-      READY: "ready",
-    };
-  }
+    static get events() {
+        return {
+            READY: "ready",
+        };
+    }
 
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
-    let count = 0;
+        this._beat = new Beat();
+        this._create();
 
-    const message = document.createElement("div");
-    message.classList.add("message");
-    message.innerText = "Ah";
-
-    document.querySelector(".main").appendChild(message);
-
-    this.emit(Application.events.READY);
-  }
+        this.emit(Application.events.READY);
+    }
+    _create() {
+        const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
+        let count = 0;
+        this._beat.addListener(Beat.events.BIT, () => {
+            const message = document.createElement("div");
+            message.classList.add("message");
+            message.innerText = lyrics[count];
+            count++;
+            document.querySelector(".main").appendChild(message);
+        })
+    }
 }
--- src/js/Beat.js
+++ src/js/Beat.js
@@ -1,13 +1,16 @@
-export default class Beat {
-  static get events() {
-    return {
-      BIT: "bit",
-    };
-  }
+import EventEmitter from "eventemitter3";
+export default class Beat extends EventEmitter{
+    static get events() {
+        return {
+            BIT: "bit",
+        };
+    }
 
-  constructor() {
-    setInterval(() => {
-      console.log("bit");
-    }, 600);
-  }
+    constructor() {
+        super();
+        setInterval(() => {
+            console.log("bit");
+            this.emit(Beat.events.BIT);
+        }, 600);
+    }
 }
